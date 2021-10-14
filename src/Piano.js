6b82017026282404e0f19bf0029c472bfdb55bc4
bleufox <thebleufox8@gmail.com>
2021-10-13 19:29:28 -0700

    Fixed a few bugs, started CSS styling

--- src/Piano.js
+++ src/Piano.js
@@ -61,7 +61,7 @@ const Piano = () => {
       <Instrument
         instrumentName={"acoustic_grand_piano"}
         startNote={"C3"}
-        endNote={"B5"}
+        endNote={"B3"}
         renderPianoKey={renderPianoKey}
         keyboardMap={{
           Q: "C3",
@@ -75,31 +75,7 @@ const Piano = () => {
           6: "G#3",
           Y: "A3",
           7: "A#3",
-          U: "B3",
-          I: "C4",
-          9: "C#4",
-          O: "D4",
-          0: "D#4",
-          P: "E4",
-          Z: "F4",
-          S: "F#4",
-          X: "G4",
-          D: "G#4",
-          C: "A4",
-          F: "A#4",
-          V: "B4",
-          B: "C5",
-          H: "C#5",
-          N: "D5",
-          J: "D#5",
-          M: "E5",
-          ",": "F5",
-          L: "F#5",
-          ".": "G5",
-          ";": "G#5",
-          "/": "A5",
-          "'": "A#5",
-          A: "B5"
+          U: "B3"
         }}
       />
     </div>
