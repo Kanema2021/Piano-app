81fa8f8c688ebf00e7330fe4dd8c409582ff4318
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-10-16 14:06:44 -0700

    Included resolvers and type defs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,10 @@
+import { users } from "./User"
+
 const resolvers = db => ({
     Query: {
-        
+        user(parent, { id }){
+            return users.find(user => user.id === id);
+        }
     },
 });
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,14 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql   `
+    type User {
+        id: ID!
+        name: String
+    }
 
+    type Query {
+        user(id: ID!): User
+    }
 `;
 
 module.exports = typeDefs;
