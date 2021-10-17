9cee9ba73c8a91d986443783e93077e381d3a3e7
Karen Marting <karenannemarting@yahoo.com>
2021-10-17 14:54:52 -0700

    login query and mutations created. Login link still not working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+// ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/utils/queries.js
@@ -0,0 +1,14 @@
+import gql from "graphql-tag";
+
+export const GET_ME = gql`
+ {
+    me {
+        _id
+        username
+        email
+        
+        }
+
+    }
+}
+`;
