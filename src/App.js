7389c49ac10bc75a32de7afe8d7ee1d5fb9cb13a
Karen Marting <karenannemarting@yahoo.com>
2021-10-18 19:53:33 -0700

    updated login code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client'
+import Navbar from './components/Navbar';
 import "./App.css";
 import Piano from "./Piano";
+import { BrowserRouter } from 'react-router-dom';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  request: operation => {
+    const token = localStorage.getItem("id-token")
+    operation.setContext(({ headers = {} }) => ({
+      headers: {
+        ...headers,
+        authorization: localStorage.getItem(token) || "",
+      }
+    }))
+  }
+})
+
 function App() {
   return (
-    <div className="app-container">
-      <Piano />
-    </div>
+
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Navbar />
+        <div className="app-container">
+          <Piano />
+        </div>
+      </BrowserRouter>
+      
+    </ApolloProvider>
+
   );
 }
-
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+
+ReactDOM.render(
+    <React.StrictMode>
+      <Router>
+          {/* <Navigation /> */}
+        <App />
+      </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 
 ReactDOM.render(<App />, document.getElementById('root'));
