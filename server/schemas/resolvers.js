96585ba3e2f12bc53403fe931b30720bd6fbf199
bleufox <thebleufox8@gmail.com>
2021-10-18 23:46:12 -0700

    Troubleshooting module error

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,20 @@
-import { users } from "./User"
+// import { users } from "../models/User"
 
-const resolvers = db => ({
-    Query: {
-        user(parent, { id }, context){
-            if (!context.user) return null;
-            return users.find(user => user.id === id);
-        }
-    },
-    Mutation: {
-        createUser: async () => {
-            const user = await users.create();
-            return user;
-        }
-    }
-});
+// const resolvers = db => ({
+//     Query: {
+//         user(parent, { id }, context){
+//             if (!context.user) return null;
+//             return users.find(user => user.id === id);
+//         }
+//     },
+//     Mutation: {
+//         createUser: async () => {
+//             const user = await users.create();
+//             return user;
+//         }
+//     }
+// });
 
 
-module.exports = resolvers;
+// module.exports = resolvers;
 
